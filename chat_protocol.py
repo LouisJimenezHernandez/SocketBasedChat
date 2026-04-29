0741cb3f3aabab2089cfad7ef69e7c40ad60fc9e
LouisJimenezHernandez <louisjh@bu.edu>
2026-04-29 16:04:59 -0400

    Chat_protocol defined

---
+++ chat_protocol.py
@@ -0,0 +1,40 @@
+SEPARATOR = "|"
+
+JOIN = "JOIN"
+MSG = "MSG"
+DM = "DM"
+LIST = "LIST"
+QUIT = "QUIT"
+
+OK = "OK"
+ERROR = "ERROR"
+SERVER = "SERVER"
+FROM = "FROM"
+PRIVATE = "PRIVATE"
+USERLIST = "USERLIST"
+
+
+def build_message(command, *parts):
+    """
+    Builds a protocol message like:
+    MSG|Hello everyone
+    DM|Bob|Hello
+    """
+    return SEPARATOR.join([command, *parts])
+
+
+def parse_message(raw_message):
+    """
+    Converts a raw protocol message into:
+    command, parts
+    """
+    raw_message = raw_message.strip()
+
+    if not raw_message:
+        return "", []
+
+    pieces = raw_message.split(SEPARATOR)
+    command = pieces[0]
+    parts = pieces[1:]
+
+    return command, parts
